8a225825ecfeff84d170453d19d08b1eb5c52801
Kuldeep Sahoo <144123998+Kuldeep-Sahoo@users.noreply.github.com>
2025-01-24 21:37:21 +0530

    database changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const connectToMongo = async () => {
-    const res=await mongoose.connect("mongodb://localhost:27017/gallery")
+    const res = await mongoose.connect(
+      "mongodb+srv://gofood:Sahoo1234567890@cluster0.8o0ds.mongodb.net/gallerylocal?retryWrites=true&w=majority&appName=Cluster0"
+    );
     if (res) {
         console.log("Connected to Mongodb");
     }
