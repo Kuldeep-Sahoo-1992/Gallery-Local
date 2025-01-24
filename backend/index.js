1d1af301281256407993a2d4d64ce1c4758e70ef
Kuldeep Sahoo <144123998+Kuldeep-Sahoo@users.noreply.github.com>
2025-01-24 21:28:23 +0530

    ready for deployement

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectToMongo from "./config/db.js";
 import router from "./routes/gallery.js";
 import cors from "cors";
+import path from "path";
 
 connectToMongo();
 
@@ -12,6 +13,18 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
 app.use(express.static("public/upload"));
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+);
+// --------------------------deployment------------------------------
+
 app.listen(PORT, () => {
   console.log(`Api is running on port: ${PORT}`);
 });
